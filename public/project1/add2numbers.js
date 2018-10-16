02ac5d6c6d434ea4852eb0921238a5e402693ac0
Luqman <luqeckr@gmail.com>
2018-10-16 15:00:03 +0800

    set default value

--- public/project1/add2numbers.js
+++ public/project1/add2numbers.js
@@ -1,7 +1,7 @@
 function kalkulator() {
     let angka = document.querySelectorAll('input');
-    let i1= parseInt(angka[0].value) ; //angka pertama
-    let i2= parseInt(angka[1].value) ; //angka kedua
+    let i1= angka[0].value ? parseInt(angka[0].value) : 0; //angka pertama
+    let i2= angka[1].value ? parseInt(angka[1].value) : 0; //angka kedua
     angka[2].value= i1 + i2 ;
 }
 let tombol = document.querySelector('button');
