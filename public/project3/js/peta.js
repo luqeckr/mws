9d64a2aa1900c0ee0265dec2923b5ac7bfd4e3fb
Luqman <luqeckr@gmail.com>
2018-10-23 09:53:58 +0800

    tambah async/await fetch

--- public/project3/js/peta.js
+++ public/project3/js/peta.js
@@ -16,24 +16,30 @@ function onMapClick(e) {
 }
 mymap.on('click', onMapClick);
 
-let arr_marker= [
-    {loc: [-5.134627, 119.490707], titel: 'PTIK UNHAS', pic: 'ptik', txt: '<strong>PTIK Unhas</strong> - Kantor bersejarah<br>Lokasi: Kampus UNHAS Tamalanrea'},
-    {loc: [-5.139906, 119.488468], titel: 'Pintu 1 Unhas', pic: 'p1unhas', txt: 'Tempat jalan2/olahraga dihari libur<br>Lokasi: Pintu 1 Unhas Tamalanrea'},
-    {loc: [-5.143144, 119.490491], titel: 'Warung Sederhana', pic: 'warung_sederhana', txt: 'Warung ikan bakar murah meriah dan enak<br>Lokasi: Jl. Perintis Kemerdekaam 4, Tamalanrea'},
-    {loc: [-5.144563, 119.465105], titel: 'Tirta Lontara', pic: 'kolamrenang', txt: 'Kolam renang Tirta Lontara<br>Lokasi: Kodam VII Wirabuana Makassar'},
-    {loc: [-5.141731, 119.484666], titel: 'Sop Sodara', pic: 'sopsodara', txt: 'Sop Sodara Enak!<br>Lokasi: jl. Perintis Kemerdekaan, km. 9'},
-    // {loc: [-5.153585, 119.4385239], pic: 'pic', txt: 'Keterangan'}
-];
 function onMarkerClick(e) {
     let gambar = document.getElementById('gambar');
     let review = document.getElementById('review');
     gambar.style.backgroundImage = "url(/images/"+e.target.options[1]+"-normal.jpg)";
     review.innerHTML = e.target.options[2];
 }
-let marker;
-for (m of arr_marker) {
-    console.log(m);
-    marker = L.marker(m.loc, [m.titel, m.pic, m.txt]).addTo(mymap);
-    marker.on('click', onMarkerClick);
-    marker.bindTooltip(m.titel);
+
+function setMarker(places) {
+    let marker;
+    for (m of places) {
+        marker = L.marker(m.lokasi, [m.sponsor, m.gambar, m.review]).addTo(mymap);
+        marker.on('click', onMarkerClick);
+        marker.bindTooltip(m.sponsor);
+    }
 }
+
+(async function (){
+    const URL="data/peta.json";
+    try {
+        let resp= await(fetch(URL));
+        let resp2= await resp.json();
+        setMarker(resp2.places);
+    }
+    catch(err){
+        console.log(err);
+    }
+})( );
